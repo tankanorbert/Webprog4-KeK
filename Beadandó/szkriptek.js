de1fac6ccf2f8077e78f06e5ce795ee952aa4a87
salyijanos <55670326+salyijanos@users.noreply.github.com>
2019-10-19 16:01:49 +0200

    Beadandó
    CSS frissítve
    Legördülőmenü / léptetőgombok hozzáadva 
    Csúszka css+html 
    Alap szövegdobozok

--- Beadandó/szkriptek.js
+++ Beadandó/szkriptek.js
@@ -14,3 +14,13 @@ function openCity(evt, cityName) {
 }
 document.getElementById("defaultOpen").click();
 
+//value scripts
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
